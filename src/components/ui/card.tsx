2441dc57d0b90c4d282257e63fa9550cd10d63e3
julianjany <julianjany.1999@gmail.com>
2024-04-24 18:18:55 +0200

    adds `card.tsx`

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Icon = ({ className, ...rest }: any) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className={className}
+            {...rest}
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+        </svg>
+    );
+};
+
+export const Card = ({
+    title,
+    icon,
+    children,
+}: {
+    title: string;
+    icon: React.ReactNode;
+    children?: React.ReactNode;
+}) => {
+    const [hovered, setHovered] = React.useState(false);
+    return (
+        <div
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] relative"
+        >
+            <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+            <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+            <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+            <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+
+            <div className="relative z-20">
+                <div className="text-center w-full  mx-auto flex items-center justify-center">
+                    {icon}
+                </div>
+                <h2 className="dark:text-white text-xl relative z-10 text-black mt-4  font-bold">
+                    {title}
+                </h2>
+                {children}
+            </div>
+        </div>
+    );
+};
