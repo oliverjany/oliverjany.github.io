5e4707e200679335d7f3c0bd6545119a9339e90c
julianjany <julianjany.1999@gmail.com>
2024-04-26 16:51:21 +0200

    adds altered `animated-tooltip`

---
+++ src/components/ui/animated-tooltip.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  motion,
+  useTransform,
+  AnimatePresence,
+  useMotionValue,
+  useSpring,
+} from "framer-motion";
+
+export const AnimatedTooltip = ({
+  tooltip,
+  children,
+}: {
+  tooltip: string;
+  children: React.ReactNode;
+}) => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const springConfig = { stiffness: 100, damping: 5 };
+  const x = useMotionValue(0); // going to set this value on mouse move
+  // rotate the tooltip
+  const rotate = useSpring(
+    useTransform(x, [-100, 100], [-45, 45]),
+    springConfig
+  );
+  // translate the tooltip
+  const translateX = useSpring(
+    useTransform(x, [-100, 100], [-50, 50]),
+    springConfig
+  );
+  const handleMouseMove = (event: any) => {
+    const halfWidth = event.target.offsetWidth / 2;
+    x.set(event.nativeEvent.offsetX - halfWidth); // set the x value, which is then used in transform and rotate
+  };
+
+  return (
+    <>
+      
+        <div
+          className="relative group"
+          style={{ display: 'inline' }}
+          onMouseEnter={() => setHoveredIndex(0)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          {hoveredIndex === 0 && (
+            <motion.div
+              initial={{ opacity: 0, y: 20, scale: 0.6 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                scale: 1,
+                transition: {
+                  type: "ease-in-out",
+                  //stiffness: 260,
+                  //damping: 10,
+                },
+              }}
+              exit={{ opacity: 0, y: 20, scale: 0.6 }}
+              style={{
+                translateX: translateX,
+                rotate: rotate,
+                whiteSpace: "nowrap",
+              }}
+              className="absolute -top-10 -left-1/2 translate-x-1/2 flex text-xs  flex-col items-center justify-center rounded-md bg-black z-50 shadow-xl px-4 py-2"
+            >
+              <div className="text-white text-xs">{tooltip}</div>
+            </motion.div>
+          )}
+          <span
+            className="object-cover !m-0 !p-0 object-top "
+          >
+            {children}
+          </span>
+        </div>
+      
+    </>
+  );
+};
