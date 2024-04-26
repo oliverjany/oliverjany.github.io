d4d331937126e5ccbc7a018240e6035ec44b4139
julianjany <julianjany.1999@gmail.com>
2024-04-26 17:03:19 +0200

    adds changes to (main) `page`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,106 @@
-import Image from "next/image";
+"use client";
+
+import React from "react";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+import { Header } from "@/components/ui/header";
+import { FittingPage } from "./pages/fitting";
+import { AboutPage } from "./pages/about";
+import { CompanyPage } from "./pages/company";
+import { ContactPage } from "./pages/contact";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      { // <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        // <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        // <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        // <link rel="manifest" href="/site.webmanifest">
+        // <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        // <meta name="msapplication-TileColor" content="#da532c">
+        // <meta name="theme-color" content="#000000"></meta>
+        // </meta>
+      }
+      <Header />
+      <main>
+
+        {//<AuroraBackground />
+        }
+
+        <div
+          className="relative flex flex-col  items-center justify-center text-slate-950 overflow-hidden"
+        >
+          <div style={{ maxWidth: '140%', marginTop: 64 }} className="bg-black">
+
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+              spaceBetween={0}
+              slidesPerView={3}
+              navigation
+              // onSlideChange={() => console.log('slide change')}
+              // onSwiper={(swiper) => console.log(swiper)}
+              autoplay={{ delay: 5000, pauseOnMouseEnter: true }}
+              centeredSlides={true}
+              className="border-b border-neutral-200 dark:border-white/[0.1]"
+            >
+              <SwiperSlide>
+                <img src="_DSF1731.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="edited_clubs.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="_DSF1746.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="_DSF1747.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="_DSF1737.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="_DSF1740.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="_DSF1743.jpg" />
+              </SwiperSlide>
+
+            </Swiper>
+          </div>
+
+
+          <div className="font-extralight text-4xl dark:text-white pt-80 pb-4">
+            BODY GEOMETRY FITTING
+          </div>
+          <TextGenerateEffect words={[
+            { word: "Deine", bold: true },
+            { word: "Geometrie", bold: false },
+            { word: "-", bold: false },
+            { word: "Deine", bold: true },
+            { word: "Schläger", bold: false }
+          ]}
+            className="text-6xl pb-80"
+          />
+
+          {/*<div className="flex flex-col items-center gap-8 w-[100%] bg-black pt-16 pb-16 backdrop-blur-sm bg-white/[0.6] dark:bg-black/[0.4] border-y border-neutral-200 dark:border-white/[0.1]">*/}
+          {/*<div className="flex flex-col items-center gap-8 w-[100%] bg-black pt-12 pb-12 backdrop-blur-sm bg-white/[0.6] dark:bg-black/[0.3] border-y border-neutral-200 dark:border-white/[0.1]">
+            <img alt="Logo" loading="lazy" width="72" height="72" decoding="async" data-nimg="1" src="golfwerkstatt_logo_white.svg" style={{ color: 'transparent' }} />
+          </div>
+          */}
+
+          <FittingPage />
+          <AboutPage />
+          <CompanyPage />
+          <ContactPage />
+
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
