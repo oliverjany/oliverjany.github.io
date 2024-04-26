92568bef8c882ffafbe3185e30a01357f51e27ab
julianjany <julianjany.1999@gmail.com>
2024-04-26 17:13:13 +0200

    adds `basePath` to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: 'golfwerkstatt',
+    output: 'export',
+    reactStrictMode: true,
+};
 
 export default nextConfig;
