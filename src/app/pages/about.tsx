5bd850060dc55abadab08be6e875241821533a39
julianjany <julianjany.1999@gmail.com>
2024-09-26 11:44:31 +0200

    alters 'about' and 'industry' texts

--- src/app/pages/about.tsx
+++ src/app/pages/about.tsx
@@ -28,7 +28,7 @@ export const AboutPage = () => (
                     </div>
 
                     <p className="mt-0 mb-2">
-                        Vor nunmehr 10 Jahren bin ich durch Zufall zum Golfspiel gekommen.
+                        Vor nunmehr über 10 Jahren bin ich durch Zufall zum Golfspiel gekommen.
                         Ich war sofort mit dem Golfvirus infiziert und die Faszination,
                         einen kleinen Ball mit unterschiedlichen Schlägern mit so wenigen Schlägen als
                         möglich in ein Loch zu befördern, hat mich nicht mehr losgelassen.
@@ -41,13 +41,13 @@ export const AboutPage = () => (
                         weshalb ich wiederholt Probleme im Rückenbereich hatte.
                     </p>
                     <p className="mt-0 mb-2">
-                        Ich hatte mir zu diesem Zeitpunkt &mdash; vor nunmehr drei
-                        Jahren zum Ziel gesetzt herauszufinden, was die wesentlichen Kriterien eines Golfschlägers
-                        sind, um für meine Person passende Schläger zu erhalten.
+                        Ich hatte mir zu diesem Zeitpunkt zum Ziel gesetzt, herauszufinden, 
+                        was die wesentlichen Kriterien eines Golfschlägers sind, 
+                        um für meine Person passende Schläger zu erhalten.
                     </p>
                     <p className="mt-0 mb-0">
                         Anfangs war ich noch der Überzeugung, dass dies nicht so schwierig sein sollte.
-                        Jedoch weit gefehlt, wie sich in den nächsten Wochen und Monaten immer wieder zeigen sollte.
+                        Jedoch weit gefehlt, wie sich in den darauf folgenden Wochen und Monaten immer wieder zeigen sollte.
                         Mir wurde schnell klar, welch komplexes &bdquo;Sportgerät&ldquo; ein Golfschläger in Wirklichkeit ist.
                     </p>
                 </div>
